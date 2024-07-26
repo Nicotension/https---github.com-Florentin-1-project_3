b0fb2d0cf033103d35f2dcde732d468dcc30516f
Nicholas <nicholas.isikhueme@outlook.com>
2024-07-26 09:55:28 +0200

    All

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,6 +6,9 @@ use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class ProductType extends AbstractType
 {
@@ -17,7 +20,32 @@ class ProductType extends AbstractType
             ->add('availability')
             ->add('picture')
             ->add('category')
-            ->add('description')
+            ->add('description', )
+
+            ->add('picture', FileType::class, [
+                'label' => 'Picture (img, jpg, jpeg file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using attributes
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '3024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpg',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
         ;
     }
 
